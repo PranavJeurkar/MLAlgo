205be8d0b744abc601d64a183dd0f0e095e0ed05
PranavJeurkar <60751023+PranavJeurkar@users.noreply.github.com>
2020-05-01 19:13:22 +0530

    Add files via upload

---
+++ p34.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+x = range(0,6)
+y = [5,8,9,12,15,18]
+plt.scatter(x,y)
+
+x = range(0,5)
+y = [26,36,22,18,40]
+plt.scatter(x,y)
+
+x = range(0,2)
+y = [9.8,28.4]
+plt.scatter(x,y)
